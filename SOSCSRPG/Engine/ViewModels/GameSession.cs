be735fd0fd034e1a2c81644d8f3989e82587f43e
cdgraybill <45321575+cdgraybill@users.noreply.github.com>
2019-03-17 18:27:25 -0500

    Add files via upload

---
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+     public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Models.Player();
+            CurrentPlayer.Name = "Cameron";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 0;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
